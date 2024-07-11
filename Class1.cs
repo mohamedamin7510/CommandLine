b5fea163b3fdb868ca94217f00af4f7829e7f2f9
mohamed amin <maming7510@gmail.com>
2024-07-11 17:39:35 +0300

    First commit

---
+++ Class1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9.List
+{
+    internal static class commands
+    {
+
+        /// <summary>
+        /// he's return list of directories and files in the path
+        /// </summary>
+        /// <returns>string of files and directories</returns>
+        public static void list(string path)
+        {
+            foreach (var items in Directory.GetDirectories(path))
+            {
+                Console.WriteLine("['dir']: " + items);
+            }
+            foreach (var items in Directory.GetFiles(path))
+            {
+                Console.WriteLine("['file']: " + items);
+            }
+            Console.WriteLine("here we go: [Salad from directory and files] ");
+            foreach (var entites in Directory.GetFileSystemEntries(path))
+            {
+                Console.WriteLine(entites);
+            }
+        }
+        public static void info(string path)
+        {
+            if (File.Exists(path))
+            {
+                var infofile = new FileInfo(path);
+                Console.WriteLine("Type: File");
+                Console.WriteLine($"Full name of file: {infofile.FullName}");
+                Console.WriteLine("create time: " + infofile.CreationTime);
+                Console.WriteLine("last modfied time: " + infofile.LastWriteTime);
+                Console.WriteLine("Size of file: " + infofile.Length);
+            }
+            else if (Directory.Exists(path))
+            {
+                var infodir = new DirectoryInfo(path);
+                Console.WriteLine("Type: dir");
+                Console.WriteLine("create time: " + infodir.CreationTime);
+                Console.WriteLine("last modfied time: " + infodir.LastWriteTime);
+            }
+            else
+            {
+                Console.WriteLine("type not known");
+            }
+
+
+        }
+        public static void mkdir(string path)
+        {
+            Directory.CreateDirectory(path);
+        }
+        public static void remove(string path)
+        {
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+                Console.WriteLine("'file is deleted'");
+            }
+            else if (Directory.Exists(path))
+            {
+                try 
+                { 
+                    Directory.Delete(path);
+                }catch (Exception err)
+                {
+                    Console.WriteLine(err.Message); 
+                }
+            }
+            else { Console.WriteLine("rewrite the path"); }
+        }
+
+        public static void read(string path)
+        {
+            StreamReader line = new StreamReader(path);
+            string Data;
+            while ((Data = line.ReadLine()) != null) 
+            {
+                Console.WriteLine(Data);   
+            }
+
+        }public static void type(string path)
+        {
+            File.CreateText(path);   
+            
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using ConsoleApp9.List;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+using System.IO;
+using System.ComponentModel.Design;
+
+
+
+namespace CommandLine_CLI_
+{
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine(">> ");
+                var input = Console.ReadLine().Trim();//mohamed 
+                var whitespace = input.IndexOf(' ');
+                var command = input.Substring(0, whitespace).ToLower();
+                var path = input.Substring(whitespace + 1).Trim();
+                if (command=="list")
+                {
+                    commands.list(path);
+                }
+                else if (command=="info")
+                {
+                    commands.info(path);
+                }
+                else if(command=="mkdir")
+                {
+                    commands.mkdir(path);                 
+                }
+                else if (command=="remove")
+                {
+                   commands.remove(path);
+                 
+                }
+                else if (command=="read")
+                {
+                    commands.read(path);
+                
+                } else if (command=="type")
+                {
+                    commands.type(path);
+                
+                }
+                
+            }
+
+           
+        }
+        
+
+    }
+}
